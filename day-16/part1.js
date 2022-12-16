46aea55277b78d875267764c88dbd7ec9d936044
Chris Waters <bieoe.ffe@gmail.com>
2022-12-16 17:39:57 -0600

    Day 16 pt 1

---
+++ day-16/part1.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+
+const isSample = false
+const prefix = isSample ? 'sample.' : ''
+const inputFile = `${prefix}input.txt`
+const outputFile = `${prefix}output.txt`
+const printFile = `${prefix}print.txt`
+const logFile = `${prefix}logs.txt`
+
+const buffer = fs.readFileSync(inputFile)
+const input = buffer.toString().split('\n')
+
+const logs = []
+
+const isDebug = true
+const debug = (msg) => {
+  if (!isDebug) return
+  console.log(msg)
+}
+
+const valves = {}
+const createValve = (valve, rate, edges) => {
+  if (valves[valve]) return
+
+  valves[valve] = {}
+  valves[valve].rate = Number(rate)
+  valves[valve].edges = edges.split(', ')
+}
+const readLine = (line) => {
+  const [,valve, rate, edges] = line.match(/Valve (\w+) has flow rate=(\d+); tunnel(?:s?) lead(?:s?) to valve(?:s?) (\w+(, \w+)*)/)
+  createValve(valve, rate, edges)
+}
+
+for (const line of input) {
+  readLine(line)
+}
+
+const sp_cache = {}
+const getShortestPath = (start, end) => {
+  const cacheKey = [start, end].join(':')
+  if (!valves[start]) throw new Error(`${start} not defined`)
+  if (!valves[end]) throw new Error(`${end} not defined`)
+  if (sp_cache[cacheKey]) return sp_cache[cacheKey]
+  let q = valves[start].edges.map(edge => ({ path: '', next: edge }))
+  while (true) {
+    const {path, next} = q.shift()
+    if (next === end) {
+      const shortestPath = [...path.split(','), next]
+      sp_cache[cacheKey] = shortestPath
+      return shortestPath
+    }
+    for (const edge of valves[next].edges) {
+      if (path.split(',').includes(next)) continue
+      q.push({ path: [...path.split(','), next].join(','), next: edge })
+    }
+  }
+}
+const getFunctionalValves = () => Object.keys(valves).filter(key => valves[key].rate > 0)
+
+const limit = 30
+const dfs = (cur, openValvesOrder, turn) => {
+  if (turn > limit) return [0]
+  const results = [0]
+  const openValves = openValvesOrder.split(',')
+  const toOpen = getFunctionalValves().filter(valve => !openValves.includes(valve))
+  for (const next of toOpen) {
+    const pathLength = getShortestPath(cur, next).length
+    const tPressure = Math.max((limit - turn - pathLength + 1) * valves[next].rate, 0) + Math.max(...dfs(next, `${[...openValvesOrder.split(','), next].join(',')}`, turn + pathLength))
+    results.push(tPressure)
+  }
+  return results
+}
+console.log(Math.max(...dfs('AA', '', 1)))
